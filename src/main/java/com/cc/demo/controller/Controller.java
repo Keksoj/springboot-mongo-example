a104b26ef587cf5bdc7482929a0ab9413fb2bf88
Emmanuel Bosquet <emmanuel.bosquet@ecole404.fr>
2022-03-16 14:00:42 +0100

    hallo alexander

--- src/main/java/com/cc/demo/controller/Controller.java
+++ src/main/java/com/cc/demo/controller/Controller.java
@@ -8,7 +8,7 @@ public class Controller {
     
     @RequestMapping("/")
     public String index() {
-        return "<body><h1>Clever Cloud is very happy to meet you, Maik!</h1><p>Diese Anwendung wurde mit Springboot erstellt, mit Maven gebuildet, und verbindet sich mit einem MongoDB.</p></body>";
+        return "<body><h1>Clever Cloud is very happy to meet you, Alexander!</h1><p>Diese Anwendung wurde mit Springboot erstellt, mit Maven gebuildet, und verbindet sich mit einem MongoDB.</p></body>";
     }
 
 }
